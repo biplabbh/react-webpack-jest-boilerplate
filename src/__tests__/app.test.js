0174831e413936f624fdc2709f2e6567aad8ae93
Bhattacharya <Biplab.Bhattacharya@absa.co.za>
2018-07-17 09:59:01 +0200

    First commit

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+describe('App', () => {
+    it('should be able to run tests', () => {
+        expect(1 + 2).toEqual(3);
+    });
+});
